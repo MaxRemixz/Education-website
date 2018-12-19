4d0f42b3af1f8e8e2ce4623a6d08979632116a03
gsyz <771874135@qq.com>
2018-12-19 15:07:18 +0800

    完成了xadmin后台管理的全局配置

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,5 +1,20 @@
 import xadmin
+from xadmin import views
+
 from .models import EmailVerifyRecord, Banner
+
+
+# 配置页面元素主题
+class BaseSetting(object):
+	enable_themes = True
+	use_bootswatch = True
+
+
+class GlobalSettings(object):
+	site_title = "ed后台管理系统"
+	site_footer = "ed在线网"
+	menu_style = "accordion"
+
 
 class EmailVerifyRecordAdmin(object):
 	list_display = ['code', 'email', 'send_type', 'send_time']
@@ -14,3 +29,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
