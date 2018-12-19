d1e195dfb406d41262cc5852734e9c8fc4d02efd
Zhe Yang <771874135@qq.com>
2018-12-19 23:47:01 +0800

    配置首页和登录页面还有静态文件的设置

--- edonline/settings.py
+++ edonline/settings.py
@@ -134,3 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# 配置静态文件目录地址
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
