795a7ba3519bade179af91384ebc9fe197625e92
Zhe Yang <771874135@qq.com>
2018-12-23 00:51:25 +0800

    实现验证邮箱找回密码功能

--- apps/utils/email_send.py
+++ apps/utils/email_send.py
@@ -6,30 +6,38 @@ from edonline.settings import EMAIL_FROM
 
 
 def random_str(randomlength=8):
-	str = ''
-	chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
-	length = len(chars) - 1
-	random = Random()
-	for i in range(randomlength):
-		str += chars[random.randint(0, length)]
-	return str
+    res_str = ''
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
+    length = len(chars) - 1
+    random = Random()
+    for i in range(randomlength):
+        res_str += chars[random.randint(0, length)]
+    return res_str
 
 
 def send_register_email(email, send_type="register"):
-	email_record = EmailVerifyRecord()
-	code = random_str(16)
-	email_record.code = code
-	email_record.email = email
-	email_record.send_type = send_type
-	email_record.save()
+    email_record = EmailVerifyRecord()
+    code = random_str(16)
+    email_record.code = code
+    email_record.email = email
+    email_record.send_type = send_type
+    email_record.save()
 
-	email_title = ""
-	email_body = ""
+    email_title = ""
+    email_body = ""
 
-	if send_type == "register":
-		email_title = "ed在线网注册激活链接"
-		email_body = "请点击下面的链接激活你的账号：http://127.0.0.1:8000/active/{0}".format(code)
+    if send_type == "register":
+        email_title = "ed在线网注册激活链接"
+        email_body = "请点击下面的链接激活你的账号：http://127.0.0.1:8000/active/{0}".format(code)
 
-		send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
-		if send_status:
-			pass
+        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+        if send_status:
+            pass
+
+    elif send_type == "forget":
+        email_title = "ed在线网密码重置链接"
+        email_body = "请点击下面的链接重置你的密码：http://127.0.0.1:8000/reset/{0}".format(code)
+
+        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+        if send_status:
+            pass
