e2295e8ae608e88f94bb49d48624e69638699532
Zhe Yang <771874135@qq.com>
2018-12-22 21:14:56 +0800

    完善注册后发送邮件的链接激活

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -3,12 +3,12 @@ from captcha.fields import CaptchaField
 
 
 class LoginForm(forms.Form):
-	# required=True 代表是必填的字段 字段的名称必须和前端页面form表单的input标签的name属性值相同
-	username = forms.CharField(required=True)
-	password = forms.CharField(required=True, min_length=5)
+    # required=True 代表是必填的字段 字段的名称必须和前端页面form表单的input标签的name属性值相同
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, min_length=5)
 
 
 class RegisterForm(forms.Form):
-	email = forms.EmailField(required=True)
-	password = forms.CharField(required=True, min_length=5)
-	captcha = CaptchaField(error_messages={"invalid":"验证码错误"})
+    email = forms.EmailField(required=True)
+    password = forms.CharField(required=True, min_length=5)
+    captcha = CaptchaField(error_messages={"invalid": "验证码错误"})
--- apps/users/views.py
+++ apps/users/views.py
@@ -6,63 +6,80 @@ from django.db.models import Q
 from django.views.generic.base import View
 from django.contrib.auth.hashers import make_password
 
-from .models import UserProfile
+from .models import UserProfile, EmailVerifyRecord
 from .forms import LoginForm, RegisterForm
 from utils.email_send import send_register_email
 
 
 class CustomBackend(ModelBackend):
-	def authenticate(self, username=None, password=None, **kwargs):
-		try:
-			user = UserProfile.objects.get(Q(username=username)|Q(email=username))
-			if user.check_password(password):
-				return user
-		except Exception as e:
-			return None
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
+
+
+class ActiveUserView(View):
+    def get(self, request, active_code):
+        all_records = EmailVerifyRecord.objects.filter(code=active_code)
+        if all_records:
+            for record in all_records:
+                email = record.email
+                user = UserProfile.objects.get(email=email)
+                user.is_active = True
+                user.save()
+        return render(request, "login.html")
 
 
 class RegisterView(View):
-	def get(self, request):
-		register_form = RegisterForm()
-		return render(request, "register.html", {'register_form': register_form})
-	def post(self, request):
-		register_form = RegisterForm(request.POST)
-		if register_form.is_valid():
-			user_name = request.POST.get("email", "")
-			pass_word = request.POST.get("password", "")
-			user_profile = UserProfile()
-			user_profile.username = user_name
-			user_profile.email = user_name
-			user_profile.password = make_password(pass_word)
-			user_profile.is_active = False
-			user_profile.save()
-			send_register_email(user_name, "register")
-			return render(request, "login.html")
-		else:
-			return render(request, "register.html", {'register_form': register_form})
+    def get(self, request):
+        register_form = RegisterForm()
+        return render(request, "register.html", {'register_form': register_form})
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_name = request.POST.get("email", "")
+            pass_word = request.POST.get("password", "")
+            user_profile = UserProfile()
+            user_profile.username = user_name
+            user_profile.email = user_name
+            user_profile.password = make_password(pass_word)
+            user_profile.is_active = False
+            user_profile.save()
+            send_register_email(user_name, "register")
+            return render(request, "login.html")
+        else:
+            return render(request, "register.html", {'register_form': register_form})
 
 
 class LoginView(View):
-	def get(self, request):
-		return render(request, "login.html", {})
-	def post(self, request):
-		login_form = LoginForm(request.POST)
-		# is_valid 其实就是验证这个login_form实例里面的_errors私有属性是否为空
-		# 如果为空。就是没有错误信息。那么就是验证成功
-		if login_form.is_valid():
-			user_name = request.POST.get("username", "")
-			pass_word = request.POST.get("password", "")
-			# authenticate 是将用户名和密码提交到数据库发起验证。验证是否正确
-			# 实际的登录是用login方法
-			user = authenticate(username=user_name, password=pass_word)
-			if user is not None:
-				login(request, user)
-				return render(request, "index.html")
-			else:
-				return render(request, "login.html", {"msg":"用户名或密码错误!"})
-		else:
-			return render(request, "login.html", {"login_form":login_form})
+    def get(self, request):
+        return render(request, "login.html", {})
 
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        # is_valid 其实就
+        # 是验证这个login_form实例里面的_errors私有属性是否为空
+        # 如果为空。就是没有错误信息。那么就是验证成功
+        if login_form.is_valid():
+            user_name = request.POST.get("username", "")
+            pass_word = request.POST.get("password", "")
+            # authenticate 是将用户名和密码提交到数据库发起验证。验证是否正确
+            # 实际的登录是用login方法
+            user = authenticate(username=user_name, password=pass_word)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, "index.html")
+                else:
+                    return render(request, "login.html", {"msg": "用户未激活!"})
+            else:
+                return render(request, "login.html", {"msg": "用户名或密码错误!"})
+        else:
+            return render(request, "login.html", {"login_form": login_form})
 
 # 基于函数的认证方法
 # def user_login(request):
