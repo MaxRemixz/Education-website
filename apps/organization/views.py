57bfd08084147bf7bdd9cb70d9e62f5a26e5d8cb
gsyz <771874135@qq.com>
2018-12-25 11:56:13 +0800

    实现列表筛选功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -9,7 +9,28 @@ class OrgView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
         all_citys = CityDict.objects.all()
+        # 根据点击数取出最热门的三个课程来进行排序
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
 
+        # 取出筛选城市
+        city_id = request.GET.get('city', "")
+        if city_id:
+        	all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        category = request.GET.get('ct', "")
+        if category:
+        	all_orgs = all_orgs.filter(category=category)
+
+        # 学习人数 课程数
+        sort = request.GET.get('sort', "")
+        if sort:
+        	if sort == "students":
+        		all_orgs = all_orgs.order_by("-students")
+        	elif sort == "courses":
+        		all_orgs = all_orgs.order_by("-course_nums")
+
+        org_nums = all_orgs.count()
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
@@ -22,4 +43,9 @@ class OrgView(View):
         return render(request, 'org-list.html', {
             "all_orgs": orgs,
             "all_citys": all_citys,
+            "city_id": city_id,
+            "category": category,
+            "org_nums": org_nums,
+            "hot_orgs": hot_orgs,
+            "sort": sort,
         })
