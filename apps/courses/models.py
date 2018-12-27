a0ac4a2f74dfd5e82acd70f9e8e9d21154934db3
gsyz <771874135@qq.com>
2018-12-27 17:11:24 +0800

    完成课程详情上半部数据的填充

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -17,10 +17,20 @@ class Course(models.Model):
     image = models.ImageField(upload_to="courses/%Y/%m", verbose_name="封面图", max_length=100)
     click_nums = models.IntegerField(default=0, verbose_name="点击数")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+    category = models.CharField(verbose_name="课程类别", max_length=20, default="后端开发")
 
     class Meta:
         verbose_name = "课程"
         verbose_name_plural = verbose_name
+
+    def get_zj_nums(self):
+        # 获取课程章节数量
+        return self.lesson_set.all().count()
+
+    def get_learn_users(self):
+        # 获取学习本课程的用户
+        return self.usercourse_set.all()[0:5]
+
 
     def __str__(self):
         return self.name
