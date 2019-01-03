1f5510da11e725c42f967e836d0b9e3d27d93ade
Zhe Yang <771874135@qq.com>
2019-01-03 23:03:38 +0800

    完善个人中心修改密码功能

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
@@ -136,6 +137,9 @@ class ResetView(View):
 
 
 class ModifyPwdView(View):
+    """
+    修改用户密码
+    """
     def post(self, request):
         modify_form = ModifyPwdForm(request.POST)
         if modify_form.is_valid():
@@ -177,3 +181,23 @@ class UploadImageView(LoginRequiredMixin, View):
             return HttpResponse('{"status": "success"}', content_type='application/json')
         else:
             return HttpResponse('{"status": "fail"}', content_type='application/json')
+
+
+class UpdatePwdView(LoginRequiredMixin, View):
+    """
+    在个人中心修改密码
+    """
+    def post(self, request):
+        modify_form = ModifyPwdForm(request.POST)
+        if modify_form.is_valid():
+            pwd1 = request.POST.get("password1")
+            pwd2 = request.POST.get("password2")
+            if pwd1 != pwd2:
+                return HttpResponse('{"status": "fail", "msg": "密码不一致"}', content_type='application/json')
+            user = request.user
+            user.password = make_password(pwd1)
+            user.save()
+
+            return HttpResponse('{"status": "success"}', content_type='application/json')
+        else:
+            return HttpResponse(json.dumps(modify_form.errors), content_type='application/json')
