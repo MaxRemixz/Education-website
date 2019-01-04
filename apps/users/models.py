b6141860362e84e6aacf3905f196645b604f16d0
gsyz <771874135@qq.com>
2019-01-04 10:28:08 +0800

    个人中心发送邮箱验证码以及验证完成修改邮箱

--- apps/users/models.py
+++ apps/users/models.py
@@ -24,7 +24,7 @@ class UserProfile(AbstractUser):
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name="验证码")
     email = models.EmailField(max_length=50, verbose_name="邮箱")
-    send_type = models.CharField(verbose_name="验证码类型", choices=(("register", "注册"), ("forget", "找回密码")), max_length=10)
+    send_type = models.CharField(verbose_name="验证码类型", choices=(("register", "注册"), ("forget", "找回密码"), ("update_email", "修改邮箱")), max_length=30)
     # 如果用datetime.now() 加上括号的话就会用EmailVerifyRecord 这个model的默认生成时间作为时间
     send_time = models.DateTimeField(verbose_name="发送时间", default=datetime.now)
 
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 
-from .views import UserinfoView, UploadImageView, UpdatePwdView
+from .views import UserinfoView, UploadImageView, UpdatePwdView, SendEmailCodeView, UpdateEmailView
 
 
 urlpatterns = [
@@ -12,4 +12,10 @@ urlpatterns = [
 
 	# 用户个人中心修改密码
 	url(r'^update/pwd/$', UpdatePwdView.as_view(), name="update_pwd"),
+
+	# 发送邮箱验证码
+	url(r'^sendemail_code/$', SendEmailCodeView.as_view(), name="sendemail_code"),
+
+	# 验证邮箱验证码 并 修改邮箱
+	url(r'^update_email/$', UpdateEmailView.as_view(), name="update_email"),
 ]
